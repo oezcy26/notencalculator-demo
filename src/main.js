6eb1a30125537b0007c05901435c34a7259ef996
ALO <oezcan.altin@tfbern.ch>
2020-04-03 15:50:56 +0200

    bootsrap-vue installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router.js'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 //import here the components which are used for routing
 
